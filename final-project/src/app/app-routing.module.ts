c1c5a75b41f155c6efc28c8ffad9f0f3e44f3742
ghazalaamin <ghazalaamin780@gmail.com>
2021-03-06 19:33:54 +0530

    css

--- final-project/src/app/app-routing.module.ts
+++ final-project/src/app/app-routing.module.ts
@@ -18,35 +18,42 @@ import { QuestionDeleteComponent } from './question-delete/question-delete.compo
 import { QuestionDisplayComponent } from './question-display/question-display.component';
 import { QuestionStoreComponent } from './question-store/question-store.component';
 import { QuestionUpdateComponent } from './question-update/question-update.component';
+import { QuestionComponent } from './question/question.component';
+import { SubjectComponent } from './subject/subject.component';
 
 const routes: Routes = [
-{path:"\store",component:QuestionStoreComponent},
-{path:"\get",component: QuestionDisplayComponent},
-{path:"\Update",component:QuestionUpdateComponent},
-{path:"\delete",component: QuestionDeleteComponent},
+
 {path:"\login",component:LoginComponent,children:[
-  {path:"signup",component:SignupComponent}
+  {path:"\signup",component:SignupComponent}
 ]},
 {path:"",redirectTo:"\login",pathMatch:"full"},
  // {path:"**",component:NocomponentComponent},
 {path:"\admindashboard",component:DashboardComponent,canActivate:[MyGaurds],children:[
-
-  {path:"student",component:StudentComponent,children:[
+ {path:"student",component:StudentComponent,children:[
     {path:"DeleteStudent",component:StudentDeleteComponent},
     {path:"DisplayStudents",component:StudentDisplayComponent},
     {path:"AddStudent",component:StudentInsertComponent},
     {path:"UpdateStudent",component:StudentUpdateComponent}
-  ]}
-
- 
+  ]},
+  {path:"question",component:QuestionComponent,children:[
+    {path:"store",component:QuestionStoreComponent},
+    {path:"get",component: QuestionDisplayComponent},
+    {path:"Update",component:QuestionUpdateComponent},
+    {path:"delete",component: QuestionDeleteComponent}
+  ]},
+  {path:"subject",component:SubjectComponent,children:[
+    {path:"UpdateStudent",component:StudentUpdateComponent}
+    ]}
 ]},
+
 {path:"\studentdasboard",component:StudentDashboardComponent,canActivate:[MyGaurds],children:[
+  {path:"subject",component:SubjectComponent,children:[
   {path:"UpdateStudent",component:StudentUpdateComponent}
+  ]}
 ]},
+{path:"\home",component:DashboardComponent},
+{path:"\signup",component:SignupComponent}
 
-{path:"\login",component:LoginComponent},
-{path:"\signup",component:SignupComponent},
-{path:"\home",component:DashboardComponent}
 ]
 
 
